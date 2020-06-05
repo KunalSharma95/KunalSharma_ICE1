f806b1aefaf29fa75b74200da324358c735ee8e5
KunalSharma95 <66033989+KunalSharma95@users.noreply.github.com>
2020-06-05 17:16:26 -0400

    Update CardGameICE1.java

--- CardGameICE1.java
+++ CardGameICE1.java
@@ -1,37 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package cardgameice1;
 
-/**
- *
- * @author srinivsi
- print 7 cards (values,suits) and takes input 
- then prints if matching card is found in an array
- */
-public class CardGameICE1
+
+
+ 
+ // @author Kunal Sharma
+
+
+import java.util.Random; // needed for randomization
+public class Card 
 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args)
-    {
-        // code to generate 7 random cards and store in array
-        Card[] magicHand = new Card[7];//array of object
-        for (int i=0;i<magicHand.length;i++)
-        {
-           Card c= new Card();//object
-         //  c.setValue(method to generate random value from 1 to 13);
-         //  c.setSuits(method random suits);
-           magicHand[i]=c;//saving object in array
-        }
-    //take input from user and compare with array 
-   // value=
-       //     suit =
+  private String suit;
+  private String value; //works better as a String than as an int
+public static final String[] SUITS={"diamonds","clubs","spades","hearts"};
+public static final String[] values = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};   
+     Random r= new Random(); // initially I tried Math.random()*4 but that didn't work
+    int randomValue=r.nextInt(SUITS.length); // created 2 variables that run through each array as indexes.
+    int randomValue2 = r.nextInt(values.length);
     
+
+public String getSuits() {
+    suit = SUITS[randomValue];   
+    return suit;
+    }
+
+    
+    public void setSuits(String suit) {
+        suit = SUITS[randomValue];
+        this.suit = suit;
+        
     }
     
+    public String getValue() {
+       value = values[randomValue2];  
+        return value;
+    }
+  
+    public void setValue(String value) {
+     value = values[randomValue2];  
+        this.value = value;
+    }
+  
+    public @Override String toString()
+    {
+        return this.value + " of " + this.suit;
+    }
+      
+    
 }
+
